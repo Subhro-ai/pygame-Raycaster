b29e25cc64c941cc34af30050678573f53bb4e01
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-02-05 22:23:48 +0530

    player movement fixed

--- raycaster.py
+++ raycaster.py
@@ -22,11 +22,19 @@ square_position_y = 0
 # player
 player_x = 120
 player_y = 120
+player_speed = 2
 player = pygame.image.load('player.png').convert_alpha()
 player = pygame.transform.rotozoom(player, 0, 0.1)
-player_rect = player.get_rect(midbottom = (player_x, player_y))
 
-
+def drawLevel():
+    count = 0
+    for row in range(9):
+        for col in range(16):
+            if level_map[count] == 1:
+                x = col * (square_size)
+                y = row * (square_size)
+                pygame.draw.rect(screen, WHITE, (x, y, square_size - 5, square_size - 5))
+            count += 1
 
 level_map = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
              1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -44,24 +52,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                     player_rect.left -= 2
-                if event.key == pygame.K_s:
-                     player_rect.bottom += 2
-                if event.key == pygame.K_d:
-                     player_rect.right += 2
-                if event.key == pygame.K_w:
-                     player_rect.top -= 2
+        keys = pygame.key.get_pressed()
+
+    # Update player position based on keys
+    if keys[pygame.K_w]:
+        player_y -= player_speed
+    if keys[pygame.K_s]:
+        player_y += player_speed
+    if keys[pygame.K_a]:
+        player_x -= player_speed
+    if keys[pygame.K_d]:
+        player_x += player_speed
     screen.fill((0,0,0))
-    count = 0
-    for row in range(9):
-        for col in range(16):
-            if level_map[count] == 1:
-                x = col * (square_size)
-                y = row * (square_size)
-                pygame.draw.rect(screen, WHITE, (x, y, square_size - 5, square_size - 5))
-            count += 1
+    drawLevel()
     # pygame.draw.circle(screen, RED, (500, 500), 25)
+    player_rect = player.get_rect(midbottom = (player_x, player_y))
     screen.blit(player, player_rect)
     pygame.display.update()
+    pygame.time.Clock().tick(60)
